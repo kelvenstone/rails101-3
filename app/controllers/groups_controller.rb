f2fdf74238a5682378a36962a64e2958b14415c8
kelven <kelvenstone@163.com>
2017-04-06 23:34:26 +0800

    add permission check show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -24,17 +26,12 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert:"You have no permission"
-    end
+
+
   end
 
   def update
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert:"You have no permission"
-    end
+
     if @group.update(group_params)
       redirect_to groups_path, notice:'Update success'
     else
@@ -43,10 +40,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-      @group = Group.find(params[:id])
-      if current_user != @group.user
-        redirect_to root_path, alert:"You have no permission"
-      end 
 
       @group.destroy
       redirect_to groups_path, alert: 'Group deleted'
@@ -58,5 +51,13 @@ class GroupsController < ApplicationController
     def group_params
       params.require(:group).permit(:title, :description )
     end
+
+    def find_group_and_check_permission
+      @group = Group.find(params[:id])
+      if current_user != @group.user
+        redirect_to root_path, alert:"You have no permission"
+      end
+    end
+
 
 end
